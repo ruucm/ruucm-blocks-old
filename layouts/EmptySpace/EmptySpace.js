8c4b3818683e5ac88245135bcf81b1a76304ce24
ruucm <ruucm.a@gmail.com>
2018-07-17 14:46:10 +0900

    add mHeight to emptyspace

--- layouts/EmptySpace/EmptySpace.js
+++ layouts/EmptySpace/EmptySpace.js
@@ -1,13 +1,17 @@
-var _templateObject = _taggedTemplateLiteral(['\n  ', ';\n'], ['\n  ', ';\n']),
-    _templateObject2 = _taggedTemplateLiteral(['\n      height: ', 'px;\n    '], ['\n      height: ', 'px;\n    ']);
+var _templateObject = _taggedTemplateLiteral(['\n  ', ';\n  ', ';\n'], ['\n  ', ';\n  ', ';\n']),
+    _templateObject2 = _taggedTemplateLiteral(['\n      height: ', 'px;\n    '], ['\n      height: ', 'px;\n    ']),
+    _templateObject3 = _taggedTemplateLiteral(['\n      ', ';\n    '], ['\n      ', ';\n    ']),
+    _templateObject4 = _taggedTemplateLiteral(['\n        height: ', 'px;\n      '], ['\n        height: ', 'px;\n      ']);
 
 function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
 import React from 'react';
 import styled, { css } from 'styled-components';
-
+import media from '../../tools/media';
 var EmptySpaceWrapper = styled.div(_templateObject, function (props) {
   return props.height && css(_templateObject2, props.height);
+}, function (props) {
+  return props.mHeight && css(_templateObject3, media.phone(_templateObject4, props.mHeight));
 });
 
 var EmptySpace = function EmptySpace(props) {
--- src/layouts/EmptySpace/EmptySpace.js
+++ src/layouts/EmptySpace/EmptySpace.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
-
+import media from '../../tools/media'
 const EmptySpaceWrapper = styled.div`
   ${props =>
     props.height &&
     css`
       height: ${props.height}px;
+    `};
+  ${props =>
+    props.mHeight &&
+    css`
+      ${media.phone`
+        height: ${props.mHeight}px;
+      `};
     `};
 `
 
