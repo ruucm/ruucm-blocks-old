ce5d9045d78627c054ac654b621eef3f11b5d0e9
ruucm <ruucm.a@gmail.com>
2018-08-17 07:46:38 +0900

    triming font style

--- src/css-patterns/icons/Beerspick2Icon/Style.js
+++ src/css-patterns/icons/Beerspick2Icon/Style.js
@@ -20,319 +20,319 @@ const Style = css`
   }
 
   .beerspick-2-youtube:before {
-    content: '\e900';
+    content: '\\e900';
   }
   .beerspick-2-beerspickLogo_black .path1:before {
-    content: '\e901';
+    content: '\\e901';
     color: rgb(34, 30, 31);
   }
   .beerspick-2-beerspickLogo_black .path2:before {
-    content: '\e902';
+    content: '\\e902';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path3:before {
-    content: '\e903';
+    content: '\\e903';
     margin-left: -0.8388671875em;
     color: rgb(34, 30, 31);
   }
   .beerspick-2-beerspickLogo_black .path4:before {
-    content: '\e904';
+    content: '\\e904';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path5:before {
-    content: '\e905';
+    content: '\\e905';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path6:before {
-    content: '\e906';
+    content: '\\e906';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path7:before {
-    content: '\e907';
+    content: '\\e907';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path8:before {
-    content: '\e908';
+    content: '\\e908';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path9:before {
-    content: '\e909';
+    content: '\\e909';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path10:before {
-    content: '\e90a';
+    content: '\\e90a';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path11:before {
-    content: '\e90b';
+    content: '\\e90b';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path12:before {
-    content: '\e90c';
+    content: '\\e90c';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path13:before {
-    content: '\e90d';
+    content: '\\e90d';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path14:before {
-    content: '\e90e';
+    content: '\\e90e';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path15:before {
-    content: '\e90f';
+    content: '\\e90f';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path16:before {
-    content: '\e910';
+    content: '\\e910';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path17:before {
-    content: '\e911';
+    content: '\\e911';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path18:before {
-    content: '\e912';
+    content: '\\e912';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path19:before {
-    content: '\e913';
+    content: '\\e913';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path20:before {
-    content: '\e914';
+    content: '\\e914';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path21:before {
-    content: '\e915';
+    content: '\\e915';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path22:before {
-    content: '\e916';
+    content: '\\e916';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path23:before {
-    content: '\e917';
+    content: '\\e917';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path24:before {
-    content: '\e918';
+    content: '\\e918';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path25:before {
-    content: '\e919';
+    content: '\\e919';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path26:before {
-    content: '\e91a';
+    content: '\\e91a';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path27:before {
-    content: '\e91b';
+    content: '\\e91b';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path28:before {
-    content: '\e91c';
+    content: '\\e91c';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path29:before {
-    content: '\e91d';
+    content: '\\e91d';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path30:before {
-    content: '\e91e';
+    content: '\\e91e';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path31:before {
-    content: '\e91f';
+    content: '\\e91f';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path32:before {
-    content: '\e920';
+    content: '\\e920';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path33:before {
-    content: '\e921';
+    content: '\\e921';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path34:before {
-    content: '\e922';
+    content: '\\e922';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path35:before {
-    content: '\e923';
+    content: '\\e923';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path36:before {
-    content: '\e924';
+    content: '\\e924';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path37:before {
-    content: '\e925';
+    content: '\\e925';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path38:before {
-    content: '\e926';
+    content: '\\e926';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path39:before {
-    content: '\e927';
+    content: '\\e927';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path40:before {
-    content: '\e928';
+    content: '\\e928';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path41:before {
-    content: '\e929';
+    content: '\\e929';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path42:before {
-    content: '\e92a';
+    content: '\\e92a';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path43:before {
-    content: '\e92b';
+    content: '\\e92b';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path44:before {
-    content: '\e92c';
+    content: '\\e92c';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path45:before {
-    content: '\e92d';
+    content: '\\e92d';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path46:before {
-    content: '\e92e';
+    content: '\\e92e';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path47:before {
-    content: '\e92f';
+    content: '\\e92f';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path48:before {
-    content: '\e930';
+    content: '\\e930';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path49:before {
-    content: '\e931';
+    content: '\\e931';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path50:before {
-    content: '\e932';
+    content: '\\e932';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path51:before {
-    content: '\e933';
+    content: '\\e933';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path52:before {
-    content: '\e934';
+    content: '\\e934';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path53:before {
-    content: '\e935';
+    content: '\\e935';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path54:before {
-    content: '\e936';
+    content: '\\e936';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path55:before {
-    content: '\e937';
+    content: '\\e937';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path56:before {
-    content: '\e938';
+    content: '\\e938';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path57:before {
-    content: '\e939';
+    content: '\\e939';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path58:before {
-    content: '\e93a';
+    content: '\\e93a';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path59:before {
-    content: '\e93b';
+    content: '\\e93b';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path60:before {
-    content: '\e93c';
+    content: '\\e93c';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path61:before {
-    content: '\e93d';
+    content: '\\e93d';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_black .path62:before {
-    content: '\e93e';
+    content: '\\e93e';
     margin-left: -0.8388671875em;
     color: rgb(255, 255, 255);
   }
   .beerspick-2-beerspickLogo_white:before {
-    content: '\e93f';
+    content: '\\e93f';
     color: #fff;
   }
 `
